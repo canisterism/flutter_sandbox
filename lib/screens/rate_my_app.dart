6f38b07ce0a73a2a5536a1983a2be8e0bf5eebc8
canisterism <ohtthstc@gmail.com>
2020-07-29 19:51:52 +0900

    :sparkles: implment button to rate arbitrary app you want

--- lib/screens/rate_my_app.dart
+++ lib/screens/rate_my_app.dart
@@ -53,10 +53,53 @@ class ContentWidget extends StatefulWidget {
 }
 
 class _ContentWidgetState extends State<ContentWidget> {
+  RateMyApp arbitraryRateMyAppController = RateMyApp(
+      appStoreIdentifier: 'YOUR APP ID', googlePlayIdentifier: 'YOUR APP ID');
   List<DebuggableCondition> debuggableConditions = [];
 
   /// Whether the dialog should be opened.
   bool shouldOpenDialog = false;
+
+  void openStarRateDialog() async {
+    arbitraryRateMyAppController.init().then((value) =>
+        arbitraryRateMyAppController.showStarRateDialog(context,
+            title: 'Rate this app', // The dialog title.
+            message:
+                'You like this app ? Then take a little bit of your time to leave a rating :', // The dialog message.
+            // contentBuilder: (context, defaultContent) => content, // This one allows you to change the default dialog content.
+            actionsBuilder: (context, stars) {
+              // Triggered when the user updates the star rating.
+              return [
+                // Return a list of actions (that will be shown at the bottom of the dialog).
+                FlatButton(
+                  child: Text('OK'),
+                  onPressed: () async {
+                    print('Thanks for the ' +
+                        (stars == null ? '0' : stars.round().toString()) +
+                        ' star(s) !');
+                    // You can handle the result as you want (for instance if the user puts 1 star then open your contact page, if he puts more then open the store page, etc...).
+                    // This allows to mimic the behavior of the default "Rate" button. See "Advanced > Broadcasting events" for more information :
+                    await arbitraryRateMyAppController
+                        .callEvent(RateMyAppEventType.rateButtonPressed);
+                    Navigator.pop<RateMyAppDialogButton>(
+                        context, RateMyAppDialogButton.rate);
+                  },
+                ),
+              ];
+            },
+            ignoreIOS:
+                false, // Set to false if you want to show the native Apple app rating dialog on iOS.
+            dialogStyle: DialogStyle(
+              // Custom dialog styles.
+              titleAlign: TextAlign.center,
+              messageAlign: TextAlign.center,
+              messagePadding: EdgeInsets.only(bottom: 20),
+            ),
+            starRatingOptions:
+                StarRatingOptions(), // Custom star bar rating options.
+            onDismissed: () => arbitraryRateMyAppController
+                .callEvent(RateMyAppEventType.laterButtonPressed)));
+  }
 
   @override
   void initState() {
@@ -101,6 +144,13 @@ class _ContentWidgetState extends State<ContentWidget> {
               onPressed: () async {
                 await widget.rateMyApp
                     .reset(); // We reset all Rate my app conditions values.
+                refresh();
+              },
+            ),
+            RaisedButton(
+              child: const Text('Rate an Arbitrary App with stars'),
+              onPressed: () {
+                openStarRateDialog();
                 refresh();
               },
             ),
